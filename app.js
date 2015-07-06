d85e7d581c90f08c33de2bbaa08204efc8296c42
Keith Williams Jr <keithwilliamsjr@gmail.com>
2015-07-06 17:13:57 -0400

    Add adding ability and styles

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 var express = require('express');
 var path = require('path');
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use('/', express.static(path.join(__dirname, 'views')));
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/movies', function(req, res){
 	fs.readFile('data.json', function(err, data) {
 	    res.setHeader('Cache-Control', 'no-cache');
 	    res.json(JSON.parse(data)['movies']);
 	  });
+});
+
+app.post('/movies', function(req, res) {
+  fs.readFile('data.json', function(err, data) {
+    var movies = JSON.parse(data)['movies'];
+    movies.push(req.body);
+    fs.writeFile('data.json', JSON.stringify({"movies":movies}), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(movies);
+    });
+  });
 });
 
 app.listen(app.get('port'), function(){
--- public/scripts/movies.js
+++ public/scripts/movies.js
@@ -17,8 +17,7 @@ var MovieList = React.createClass({
 	render: function() {
 	    var movieNodes = this.props.data.map(function(movie, index) {
 	      return (
-	        // `key` is a React-specific concept and is not mandatory for the
-	        // purpose of this tutorial. if you're curious, see more here:
+	        // `key` is a React-specific concept:
 	        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
 	        <Movie time={movie.time} title={movie.title} key={index}>
 	        </Movie>
@@ -31,11 +30,35 @@ var MovieList = React.createClass({
 	    );
 }});
 
+var MovieForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var title = React.findDOMNode(this.refs.title).value.trim();
+    var time = React.findDOMNode(this.refs.time).value.trim();
+    if (!time || !title) {
+      return;
+    }
+    this.props.onMovieSubmit({title: title, time: time});
+    React.findDOMNode(this.refs.title).value = '';
+    React.findDOMNode(this.refs.time).value = '';
+  },
+  render: function() {
+    return (
+      <form className="movieForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Movie Title" ref="title" />
+        <input type="text" placeholder="When they say it" ref="time" />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
 var MovieBox = React.createClass({
 	render: function(){
 		return (
 			<div className="movies-box"> 
 				<MovieList data={this.state.data}></MovieList>
+        		<MovieForm onMovieSubmit={this.addMovie} />
 			</div>);
 	},
 	grabMovies: function(){
@@ -51,14 +74,38 @@ var MovieBox = React.createClass({
 	      }.bind(this)
 	    });
 	},
+	addMovie: function(movie) {
+		// if (this.state.auth === false) return;
+	    var movies = this.state.data;
+	    movies.push(movie);
+	    this.setState({data: movies}, function() {
+	      // `setState` accepts a callback. To avoid (improbable) race condition,
+	      // `we'll send the ajax request right after we optimistically set the new
+	      // `state.
+	      $.ajax({
+	        url: this.props.url,
+	        dataType: 'json',
+	        type: 'POST',
+	        data: movie,
+	        success: function(data) {
+	          this.setState({data: data});
+	        }.bind(this),
+	        error: function(xhr, status, err) {
+	          console.error(this.props.url, status, err.toString());
+	        }.bind(this)
+	      });
+	    });
+  	},
 	getInitialState: function() {
-	    return {data: []};
+	    return {data: [], auth: false};
 	},
 	componentDidMount: function() {
 		this.grabMovies();
-		setInterval(this.grabMovies, 3000);
+		// setInterval(this.grabMovies, 3000);
 	}
 });
+
+// Render Errrthing!
 
 React.render(
   <MovieBox url="movies"></MovieBox>,
