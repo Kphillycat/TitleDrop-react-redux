b893eb74b563a092595f1b8323bb0f9e2c8c6925
Keith Williams Jr <keithwilliamsjr@gmail.com>
2015-07-13 16:11:22 -0400

    Add gulp to da mix

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp  = require('gulp'),
+	watch = require('gulp-watch'),
+	less  = require("gulp-less"),
+	minifyCSS = require('gulp-minify-css');
+
+gulp.task('mini-less', function() {
+    var files = 'public/styles/less/*.less';
+    return gulp.src(files)
+        .pipe(watch(files))
+        .pipe(less())
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('public/styles/css'));
+});
